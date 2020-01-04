7dc73fd811e522e5911564d6197b390953638e29
albert123720 <54534420+albert123720@users.noreply.github.com>
2020-01-04 13:57:36 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -15,8 +15,8 @@ class adlogin
 		JLabel l1=new JLabel("USER NAME");
 		JLabel l2=new JLabel("PASSWORD");
 		JButton b1=new JButton("LOGIN");
-                                      b1.addActionListener (new ActionListener ()
-		{	
+                b1.addActionListener (new ActionListener ()
+	         {	
 	                   public void actionPerformed(ActionEvent e)
                                       {
                                    	  String un= tf1.getText();
@@ -25,27 +25,27 @@ class adlogin
                                    	       {
                                     		JOptionPane.showMessageDialog(f,"success"); 
                                                           new cosdetails();		
-		        }
-                               	    else
+		                               }
+                               	           else
                                              {
                               	                   JOptionPane.showMessageDialog(f,"invalid password");
-                              	       } 
-		       }
-		});
+                              	             } 
+		                       }
+		 });
                             l1.setBounds(100,100,100,20);
                             l2.setBounds(100,150,100,20);
                             tf1.setBounds(200,100,80,30);
-	         tf2.setBounds(200,150,80,30);
+	                    tf2.setBounds(200,150,80,30);
                             b1.setBounds(250,200,80,30);
                             f.setLayout(null);
                             f.add(l1);
                             f.add(l2);
                             f.add(tf1);
-                           f.add(tf2);  
+                            f.add(tf2);  
                             f.add(b1); 
                             f.setSize(500,300);
                             f.setVisible(true); 
-                       }
+       }
 
        public static void main(String arg[])
       { 
